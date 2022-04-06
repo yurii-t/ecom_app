520bc91396aa2934d1f35bec0d7114e0930bad72
yodastf <yodastf@gmail.com>
2022-04-06 12:51:43 +0300

    fix get_started_page

--- lib/ui/login/enter_phone_screen/enter_phone_widget.dart
+++ lib/ui/login/enter_phone_screen/enter_phone_widget.dart
@@ -11,31 +11,55 @@ class EnterPhoneWidget extends StatefulWidget {
 class _EnterPhoneWidgetState extends State<EnterPhoneWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-                    width: MediaQuery.of(context).size.width,
-                    //375,
-                    height:197,
-                    child: Text(''),
-                    decoration: const BoxDecoration(
-                      color:Colors.purple,
-                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(300.0)),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+                      width: MediaQuery.of(context).size.width,
+                      //375,
+                      height:197,
+                      child: const Padding(
+                        padding: EdgeInsets.fromLTRB(24, 91, 39, 44),
+                        child:  Text('What Is Your Phone Number?',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white
+                        ),),
+                      ),
+                      decoration: const BoxDecoration(
+                        color:Colors.purple,
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(300.0)),
+                         gradient: LinearGradient(
+                      colors: [Color.fromRGBO(52, 40, 60, 1),Color.fromRGBO(132, 95, 161, 1),],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      ),
+                      ),
                     ),
-                  ),
-                  Text(''),
-                  TextFormField(),
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text('Next')
+                    SizedBox(height: 33,),
+                    const Text('Please enter your phone number to verify your account',
+                    style: TextStyle(
+                      fontSize:17,
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromRGBO(96, 90, 101, 1),
+
                     ),
-                    TextButton
-                    (onPressed: (){}, 
-                    child: Text('Skip'))
-        ],
+                    ),
+                    SizedBox(height: 24,),
+                    TextFormField(),
+                    ElevatedButton(
+                      onPressed: (){},
+                      child: const Text('Next')
+                      ),
+                      TextButton
+                      (onPressed: (){}, 
+                      child: const Text('Skip'))
+          ],
+        ),
+                
       ),
-              
     );
   }
 }
--- lib/ui/login/get_started_screen/get_started_widget.dart
+++ lib/ui/login/get_started_screen/get_started_widget.dart
@@ -12,116 +12,133 @@ class GetStartedWidget extends StatefulWidget {
 
 class _GetStartedWidgetState extends State<GetStartedWidget> {
   Duration _animationDuration = Duration(seconds: 1); 
-  double _top = 340.0;
+  double _top = 450.0;
  // double _bottom = 10.0;
  // double _right = 10.0;
   double _left = 150;
+  Color yellowColor = const Color.fromRGBO(231, 185, 68, 1
+);
+   // yellowColor = const Color(0xFF7B944);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: 
-          
-             
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children:[ 
-                   Container(
+    return SafeArea(
+      child: Scaffold(
+        body: 
+            
                
-                height: 760,
-                color: Colors.grey,
-             ), 
-                
-            AnimatedPositioned(
-              duration: _animationDuration,
-              top:_top,
-           //   bottom: _bottom,
-            //  right: _right,
-              left: _left,
-              
-
-              child: 
-              // Positioned(
-              //   left: _left,//150,
-              //   top:_top,//340,
-              //   child: 
-                Container(
-                  width:269,
-                  height:146,
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children:[ 
+                     Container(
+                 
+                  height: MediaQuery.of(context).size.height,
                   
-                  decoration: BoxDecoration(
-                    color:Colors.yellow.withOpacity(0.5),
-                    borderRadius: const BorderRadius.only(topLeft: Radius.circular(300.0)),
-                  ),
-                ),
-             //   ),
-            ),
-
-              Positioned (
-                top: 400,
-                child: Container(
-                  height:291,
-                  width: MediaQuery.of(context).size.width,
                   decoration: const BoxDecoration(
-                  color:Colors.purple,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0),topRight: Radius.circular(40.0)),
+                   
+                    image: DecorationImage(
+                      fit: BoxFit.fitHeight,
+                      image: AssetImage('images/start_img.gif'),
+                    )
+                    ),
+               ), 
                   
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: Column(
-                     
-                      children: [
-                        const SizedBox(height: 25,),
-                         RichText(
-                           
-                          text:const TextSpan(
-                            style: TextStyle( fontWeight: FontWeight.w700,fontSize: 25),
-                            children:[
-                               TextSpan(text: 'My',style:TextStyle( color:Colors.yellow)),
-                               TextSpan(text: 'Shop',style:TextStyle( color:Colors.white)),
-                            ]
-                          )
-                         ),
-                         const SizedBox(height: 25,),
-                        const Text("Lorem Ipsum is simply dummy text of the  printing and typesetting industry",
-                        textAlign: TextAlign.center,
-                        style:TextStyle( color:Colors.white)),
-                        const SizedBox(height: 25,),
-                        ElevatedButton(
-                          onPressed: (){
-                           // Navigator.push(context, MaterialPageRoute(builder: (context)=> const EnterPhoneWidget()));
-                            setState(() {
-                              _top=0;
-                            //  _right=200;
-                            //  _bottom=100;
-                              _left=0;
-                            });
-                            
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary:Colors.yellow,
-                            onPrimary: Colors.white,
-                            minimumSize: const Size(239, 48),
-                          ),
-                          
-                          child: const Text('Get Started'),
-                         
-                          
-                          )
-                      ],
+              AnimatedPositioned(
+                duration: _animationDuration,
+                top:_top,
+             //   bottom: _bottom,
+              //  right: _right,
+                left: _left,
+                
+    
+                child: 
+                // Positioned(
+                //   left: _left,//150,
+                //   top:_top,//340,
+                //   child: 
+                  Container(
+                    width:269,
+                    height:146,
+                    
+                    decoration: BoxDecoration(
+                      color:Colors.yellow.withOpacity(0.7),
+                      borderRadius: const BorderRadius.only(topLeft: Radius.circular(300.0)),
                     ),
                   ),
-                ),
-              )
-                     
-            
-                ]
+               //   ),
               ),
+    
+                Positioned (
+                  top: 560,
+                //  bottom: 0,
+                  child: Container(
+                    height:291,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                    color:Colors.purple,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0),topRight: Radius.circular(40.0)),
+                     gradient: LinearGradient(
+                      colors: [Color.fromRGBO(52, 40, 60, 1),Color.fromRGBO(132, 95, 161, 1),],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      child: Column(
+                       
+                        children: [
+                          const SizedBox(height: 28,),
+                           RichText(
+                             
+                            text:const TextSpan(
+                              style: TextStyle( fontWeight: FontWeight.w700,fontSize: 31),
+                              children:[
+                                 TextSpan(text: 'My',style:TextStyle( color:Colors.yellow)),
+                                 TextSpan(text: 'Shop',style:TextStyle( color:Colors.white)),
+                              ]
+                            )
+                           ),
+                           const SizedBox(height: 24,),
+                          const Text("Lorem Ipsum is simply dummy text of the  printing and typesetting industry",
+                          textAlign: TextAlign.center,
+                          style:TextStyle( color:Colors.white,fontSize: 14)),
+                          const SizedBox(height: 32,),
+                          ElevatedButton(
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> const EnterPhoneWidget()));
+                              // setState(() {
+                              //   _top=0;
+                              // //  _right=200;
+                              // //  _bottom=100;
+                              //   _left=0;
+                              // });
+                              
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary:yellowColor,
+                              onPrimary: Colors.white,
+                              minimumSize: const Size(239, 48),
+                            ),
+                            
+                            child: const Text('Get Started',style: TextStyle(fontSize: 17),),
+                           
+                            
+                            )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+                       
+              
+                  ]
+                ),
+              
             
-          
-      
         
-      );
+          
+        ),
+    );
     
 
 
