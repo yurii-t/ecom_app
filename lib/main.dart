99371898d9af288faec12d6977b07214789aa0cc
Yura <yodastf@gmail.com>
2022-04-18 15:20:12 +0300

    add profile page

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,15 @@ import 'package:ecom_app/ui/login/enter_phone_screen/enter_phone_widget.dart';
 import 'package:ecom_app/ui/login/get_started_screen/get_started_widget.dart';
 import 'package:ecom_app/ui/login/verefication_screen/verification_widget.dart';
 import 'package:ecom_app/ui/product_page/product_page_screen/product_page_widget.dart';
+import 'package:ecom_app/ui/profile/profile_screen/profile_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- // await Firebase.initializeApp();
-  
+  // await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -28,32 +29,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
-      home:
-      ProdutPageWidget(),  
-     // FilterWidget(),
+      home: ProfileWidget(),
+      // ProdutPageWidget(),
+      // FilterWidget(),
       //ClothingWidget(),
-     // HomeWidget(),
+      // HomeWidget(),
       ////GetStartedWidget(),
-      
-    // initialRoute: '/',
-  //   routes: {
-    
-  //   '/': (context) => const GetStartedWidget(),
-    
-  //   '/enter_phone_screen': (context) => const EnterPhoneWidget(),
-  //   '/pin_virification_screen': (context) => const VerificationVWidget(phone: '',),
-  //   '/home_screen': (context) => const HomeWidget(),
-  //   '/catalogue_screen': (context) => const CatalogueWidget(),
-  //   '/categories_screen': (context) => const CategoriesWidget(),
-  //     '/clothing_screen': (context) => const ClothingWidget(),
-  //   '/filter_screen': (context) => const FilterWidget(),
-  //   '/cart_screen': (context) => const CartWidget(),
-  // },
 
+      // initialRoute: '/',
+      //   routes: {
+
+      //   '/': (context) => const GetStartedWidget(),
+
+      //   '/enter_phone_screen': (context) => const EnterPhoneWidget(),
+      //   '/pin_virification_screen': (context) => const VerificationVWidget(phone: '',),
+      //   '/home_screen': (context) => const HomeWidget(),
+      //   '/catalogue_screen': (context) => const CatalogueWidget(),
+      //   '/categories_screen': (context) => const CategoriesWidget(),
+      //     '/clothing_screen': (context) => const ClothingWidget(),
+      //   '/filter_screen': (context) => const FilterWidget(),
+      //   '/cart_screen': (context) => const CartWidget(),
+      // },
     );
   }
 }
-
--- lib/ui/profile/profile_screen/profile_widget.dart
+++ lib/ui/profile/profile_screen/profile_widget.dart
@@ -1,8 +1,9 @@
+import 'package:ecom_app/style/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileWidget extends StatefulWidget {
-  ProfileWidget({Key? key}) : super(key: key);
+  const ProfileWidget({Key? key}) : super(key: key);
 
   @override
   State<ProfileWidget> createState() => _ProfileWidgetState();
@@ -11,12 +12,212 @@ class ProfileWidget extends StatefulWidget {
 class _ProfileWidgetState extends State<ProfileWidget> {
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
-      child: Scaffold(
-        body:Center(
-          child: SizedBox(width:230,height:230,child: SvgPicture.asset('icons/profile_fill.svg',width: 200,height: 200,))),
-        
-      )
-    );
+    return SafeArea(
+        child: Scaffold(
+            body: Column(
+      // cross+AxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          //375,
+          height: 146,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 56, 0, 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 70,
+                      height: 70,
+                      decoration: const BoxDecoration(
+                          color: Colors.white, shape: BoxShape.circle),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Text(
+                          'Oleh Chabanov',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          '+38 (099) 123 45 67',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(20),
+                        primary: Colors.white),
+                    child: SvgPicture.asset('icons/pencil.svg')),
+              ],
+            ),
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.purple,
+            borderRadius:
+                BorderRadius.only(bottomRight: Radius.circular(300.0)),
+            gradient: AppColors.purpleGradient,
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/location.svg'),
+            label: const Text(
+              'Shipping Addresses',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/creditcard.svg'),
+            label: const Text(
+              'Payment Methods',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/clipboard.svg'),
+            label: const Text(
+              'Orders ',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/profile_heart.svg'),
+            label: const Text(
+              'Favorite',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/cog.svg'),
+            label: const Text(
+              'Settings',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                alignment: Alignment.centerLeft,
+                minimumSize: const Size(343, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                )),
+            icon: SvgPicture.asset('icons/logout.svg'),
+            label: const Text(
+              'Log Out',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.darkTextColor),
+            )),
+        const SizedBox(
+          height: 104,
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 36),
+            child: GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  'Privacy Policy',
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.darkTextColor),
+                )),
+          ),
+        )
+      ],
+    )));
   }
 }
